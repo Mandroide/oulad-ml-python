58f4541fed9d7efaea86daaad88d3e49085583cf
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2025-07-06 15:18:20 -0400

    Remove unnecessary environment variables from settings

--- src/oulad_etl/settings.py
+++ src/oulad_etl/settings.py
@@ -8,9 +8,6 @@ class Settings(BaseSettings):  # type: ignore[misc]
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
-    moodle_url: str = Field(alias="MOODLE_URL")
-    moodle_username: str = Field(alias="MOODLE_USERNAME")
-    moodle_password: str = Field(alias="MOODLE_PASSWORD")
     excel_absolute_path: pathlib.Path = Field(alias="EXCEL_ABSOLUTE_PATH")
 
 
